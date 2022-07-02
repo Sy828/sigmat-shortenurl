b08d21a58610c711612d02df1a9b7cad2a2573c4
Sy828 <Shwetay224@gmail.com>
2022-07-02 11:37:05 +0530

    commit for missing method name

--- app/models/link.rb
+++ app/models/link.rb
@@ -15,7 +15,8 @@ class Link < ApplicationRecord
         return url
     end
 
-    self.joins("join analytics anl on anl.link_id = links.id")
+   def self.fetch_analytics_data(baseUrl)
+        self.joins("join analytics anl on anl.link_id = links.id")
         .select("links.original_url,('#{baseUrl}' || '/' || links.lookup_code) as shorten_url,sum(anl.click_count) as no_click,anl.link_id,group_concat(DISTINCT(anl.country)) as countries,group_concat(DISTINCT(anl.state)) as states,group_concat(DISTINCT(anl.city)) as cities")
         .group("links.id")
         .order("links.id desc")
