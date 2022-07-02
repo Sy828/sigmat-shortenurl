d52d7d4b8a41f05495f6f870c5d01a0101d5d502
Shweta Yadav <shwetay224@gmail.com>
2022-07-02 12:13:47 +0530

    added secret for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.initialize_on_precompile = false
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
